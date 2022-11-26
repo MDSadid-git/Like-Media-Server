8947dd943d230c2e722a2aa21ceb13ab2d72f691
Sadid <sfhsadid@gmail.com>
2022-11-26 06:50:47 +0600

    express make done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//midell
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Resale Server is running");
+});
+
+app.listen(port, () => console.log("my Server is running!!!"));
