911ace6d7f0240d0714f9b21c14f79224d64c6ed
Sadid <sfhsadid@gmail.com>
2022-12-01 22:39:04 +0600

    full done

--- index.js
+++ index.js
@@ -119,7 +119,6 @@ async function run() {
     });
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
-      // console.log(booking);
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
     });
